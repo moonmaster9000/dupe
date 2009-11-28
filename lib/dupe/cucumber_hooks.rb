7226e7bca2fe59633a3a922f1bb8f9cedd55a00a
mparker <mparker@ml-ny-mparker.local>
2009-11-28 11:34:44 -0500

    apparently if defined?(After) doesn't work like you might expect

--- lib/dupe/cucumber_hooks.rb
+++ lib/dupe/cucumber_hooks.rb
@@ -1,10 +1,8 @@
-if defined?(After)
-  After do |scenario| #:nodoc:
-    if Dupe.global_configuration.config[:debug] == true
-      ActiveResource::Connection.print_request_log 
-      ActiveResource::Connection.flush_request_log
-    end
+After do |scenario| #:nodoc:
+  if Dupe.global_configuration.config[:debug] == true
+    ActiveResource::Connection.print_request_log 
+    ActiveResource::Connection.flush_request_log
+  end
 
-    Dupe.flush
-  end 
-end
+  Dupe.flush
+end 
